2d165cee5aedfe057641db8ea44de711356dfb1e
Josther Sampaio <josthersampaiof@gmail.com>
2024-09-23 17:53:23 -0300

    new changes in read.js

--- new-array/read.js
+++ new-array/read.js
@@ -1,40 +1,63 @@
+import { error, log } from 'console';
 import fs from 'fs';
+import { promisify } from 'util';
 
 let content;
+const readFilePromise = promisify(fs.readFile);
+const writeFilePromise = promisify(fs.writeFile);
 
-function readFile(callback){
-    fs.readFile('./CRONOGRAMA.json', 'utf8', (erro, data) => {
-        if(erro){
-            console.error(erro);
-            return;
-        } else{
-            //content = JSON.parse(data);
-            //console.log(content);
-            callback();
-            console.log(data);
-        };
+// function readFile(callback){
+//     fs.readFile('./CRONOGRAMA.json', 'utf8', (erro, data) => {
+//         if(erro){
+//             console.error(erro);
+//         } else{
+//             //content = JSON.parse(data);
+//             //console.log(content);
+//             callback();
+//             console.log(data);
+//             fs.readFile('./CRONOGRAMA.json', (erro, data) => {        
+//                 if(erro){
+//                     console.log(erro);            
+//                 }else{
+//                     console.log(JSON.parse(data));
+//                     //console.log(data);            
+//                 };
+//             })
+//         };
+//     });
+
+    // setTimeout(function(){
+    //     fs.readFile('./CRONOGRAMA.json', (erro, data) => {        
+    //         if(erro){
+    //             console.log(erro);            
+    //         }else{
+    //             console.log(JSON.parse(data));
+    //             console.log(data);            
+    //         };
+    //     })
+    // }, 2000);
+//};
+
+// function writeFile(){
+//     const newData = {'25':'concluido'};
+//     fs.writeFile('./CRONOGRAMA.json', JSON.stringify(newData), (erro) => {
+//         if(erro){
+//             console.log('deu ruim');            
+//         };
+//     });
+// };
+
+readFilePromise('./CRONOGRAMA.json', 'utf8').then((data) => {
+    console.log(data);
+    const newData = JSON.stringify({'25':'concluido'});
+    writeFilePromise('./cronograma.json', newData, 'utf8').then((data) => {
+        console.log(data);        
+    }).catch((erro) => {
+        console.error(erro);        
     });
-
-    setTimeout(function(){
-        fs.readFile('./CRONOGRAMA.json', (erro, data) => {        
-            if(erro){
-                console.log(erro);            
-            }else{
-                console.log(JSON.parse(data));
-                console.log(data);            
-            };
-        })
-    }, 2000);
-};
-
-function writeFile(){
-    const newData = {'19':'concluido'};
-    fs.writeFile('./CRONOGRAMA.json', JSON.stringify(newData), (erro) => {
-        if(erro){
-            console.log('deu ruim');            
-        };
-    });
-};
+}).catch((erro) => {
+    console.error(erro);    
+})
 
 //readFile();
-readFile(writeFile);
+//readFile(writeFile);
